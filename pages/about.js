75abdf1a41bff26328460963595e9ac0b9c19875
Parr <tedblount01@gmail.com>
2020-06-18 02:06:09 -0500

    added basic route switechs

--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,7 @@ export default () => (
 
 <div>
     <h1>About</h1>
+    <a href='/'>Home</a>
     <p>Web Dev Team</p>
     <img src="/static/mustache.jpg" alt="mustache"/>
 </div>
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,3 @@
-const Index = () => <div>hello world</div>;
+const Index = () => <div>home <a href="/about">About</a></div>;
 
 export default Index;
