a8f6b892edf12adcd5c6d3de8d33e89e9d6c1915
Parr <tedblount01@gmail.com>
2020-06-20 04:55:36 -0500

    added font

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
-import Document, { Head, Main, Next } from 'next/document';
+import Document, { Head, Main, NextScript } from 'next/document';
 
-class MyDocument extends Document {
+export default class MyDocument extends Document {
 
     render() {
         return (
@@ -8,10 +8,20 @@ class MyDocument extends Document {
                 <Head>
                 <link rel="stylesheet" 
                  href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"/>
+                 <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet"></link>
                  <meta name="description" content="A site for my programming portfolio" />
                  <meta name="robots" content="noindex, nofollow" />
                  <meta name="viewport" content="width=device-width" />
                 </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+        <style global jsx>{`
+        body {
+            font-family: 'Fredoka One', cursive;
+        }
+        `}</style>
             </html>
         )
     }
