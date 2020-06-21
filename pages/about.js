68ad4c0eb0b8618d0e9ec8d76e1a6f5fc2e37345
Parr <tedblount01@gmail.com>
2020-06-20 22:28:15 -0500

    added profile pic

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,41 @@
 import Layout from "../components/Layout";
+import fetch from "isomorphic-unfetch";
 import { Component } from 'react';
 
 export default class About extends Component {
+
+    static async getInitialProps() {
+        const res = await fetch('https://api.github.com/users/brakluner')
+        const data = await res.json();
+
+        // .then(res => res.json())
+        // .then(data => {
+        //     console.log(data)
+        // });
+
+        return { user: data};
+    }
+
+    // componentDidMount() {
+    //     fetch('https://api.github.com/users/brakluner')
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         this.setState({
+    //             user: data
+    //         })
+    //     })
+    // }
+    
+
     render() {
+        const { user } = this.props;
+
         return (
-            <Layout>
-                <img src="/static/mustache.jpg" alt="mustache"/>
+            <Layout title="About">
+                <p>{user.name}</p>
+                <img 
+                src={user.avatar_url}
+                alt="profile pic"/>
             </Layout>
         )
     }
