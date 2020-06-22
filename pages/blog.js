a45aa0255e74ac8c699052f0158f978ad6998245
Parr <tedblount01@gmail.com>
2020-06-21 21:19:33 -0500

    added blog page with corresponding post page

---
+++ pages/blog.js
@@ -0,0 +1,24 @@
+import Layout from '../components/layout';
+import Link from 'next/link';
+
+const PostLink = ({ title }) => (
+    <li>
+    <Link href={`/post?title=${title}`}>
+                    <a>
+                        {title}
+                    </a>
+    </Link>
+    </li>
+)
+
+export default () => (
+    <Layout title="My Blog">
+        <ul>
+            <li>
+                <PostLink title="Blog A"/>
+                <PostLink title="Blog B"/>
+                <PostLink title="Blog C"/>
+            </li>
+        </ul>
+    </Layout>
+)
