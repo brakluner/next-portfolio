a1e915673231d9f34349da5f37d1feb2175175b4
Parr <tedblount01@gmail.com>
2020-06-25 05:14:48 -0500

    changed hireme text

--- pages/hireme.js
+++ pages/hireme.js
@@ -2,7 +2,7 @@ import Layout from "../components/Layout"
 
 const HireMe = () => (
     <Layout title="Hire Me">
-        <h1>To Do Your Web Bidding!</h1>
+        <h1>To Do Your Web Work!</h1>
         <p>Hire Me At
             <a
             href="mailto:tedblount01@gmail.com">TedBlount01@GMAIL.COM</a>
